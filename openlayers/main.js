dd01b01fdbe3c2c046c147698f67cc42c9c71374
Edward Mac Gillavry <edward@webmapper.net>
2017-03-07 16:41:38 +0100

    OL3 renders MVT using Mapbox Style Spec.

--- openlayers/main.js
+++ openlayers/main.js
@@ -1,39 +1,26 @@
-var fill = new ol.style.Fill({
-    color: '#f00'
-});
 
-var stroke = new ol.style.Stroke({
-    color: 'rgba(0,0,0,0.4)'
-});
-
-var circle = new ol.style.Circle({
-    radius: 6,
-    fill: fill,
-    stroke: stroke
-});
-
-var vectorStyle = new ol.style.Style({
-    fill: fill,
-    stroke: stroke,
-    image: circle
+var layer = new ol.layer.VectorTile({
+	source: new ol.source.VectorTile({
+        	attributions: 'Kadaster',
+	        format: new ol.format.MVT(),
+        	tileGrid: ol.tilegrid.createXYZ({maxZoom: 18}),
+	        tilePixelRatio: 16,
+        	url: 'http://localhost/terreinvlak_3/{z}/{x}/{y}.pbf'
+	})
 });
 
 var map = new ol.Map({
-    layers: [
-        new ol.layer.VectorTile({
-            source: new ol.source.VectorTile({
-                attributions: 'Kadaster',
-                format: new ol.format.MVT(),
-                tileGrid: ol.tilegrid.createXYZ({maxZoom: 18}),
-                tilePixelRatio: 1,
-                url: 'http://localhost:6767/{z}/{x}/{y}.pbf'
-            }),
-        style: vectorStyle
-        })
-    ],
     target: 'map',
     view: new ol.View({
         center: ol.proj.fromLonLat([4.9012, 52.3719]),
-        zoom: 14
+        zoom: 7
     })
+});
+
+fetch('http://localhost/style.json').then(function(response) {
+  response.json().then(function(glStyle) {
+    olms.applyStyle(layer, glStyle, 'buildings').then(function() {
+      map.addLayer(layer);
+    });
+  });
 });
